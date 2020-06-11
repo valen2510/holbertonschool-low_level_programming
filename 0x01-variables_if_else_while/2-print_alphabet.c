0f5dd2bcffc18730618b86d301bc026bf8e026f2
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-11 17:49:42 -0500

    alphbet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * use of putchar to write characters specified by the argument
+ * Return: Always 0
+ */
+int main(void)
+{
+char al;
+for (al = 'a'; al <= 'z'; al++)
+{
+putchar(al);
+putchar('\n');
+}
+return (0);
+}
