d4ac184f31f9409725cec3a246d3d1a698990e08
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-11 19:21:01 -0500

    noeandq

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * use of putchar to write characters specified by the argument
+ * Return: Always 0
+ */
+int main(void)
+{
+char al;
+for (al = 'a'; al <= 'z'; al++)
+{
+if (al != 'e' && al != 'q')
+putchar(al);
+}
+putchar('\n');
+return (0);
+}
