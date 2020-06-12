1b4a9dffa93dec16676901f09311dec307813eef
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-11 19:52:42 -0500

    munbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * use of putchar to write characters specified by the argument
+ * Return: Always 0
+ */
+int main(void)
+{
+char al;
+for (al = '0'; al <= '9'; al++)
+putchar(al);
+putchar('\n');
+return (0);
+}
