f67bbf7b05fce7a8ad6ccba564faeb4b7de3ba10
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-11 22:15:25 -0500

    combination

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * use of putchar to write double-numbers separated for commas
+ * Return: Always 0
+ */
+int main(void)
+{
+int n;
+int n2;
+for (n = 48; n <= 57; n++)
+{
+for (n2 = 48; n2 <= 57; n2++)
+{
+putchar(n);
+putchar(n2);
+if (n != 57 || n2 != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
