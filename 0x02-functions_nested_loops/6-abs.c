0a4f81ee26db663dd02be25a40be503dea128963
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-16 11:15:26 -0500

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _abs - absolute of an integer
+ * @z: - entry value integer
+ * Return: absolute value of z
+ */
+int _abs(int z)
+{
+
+	if (z < 0)
+	{
+		z = z * (-1);
+	}
+	else
+	{
+	}
+
+	return (z);
+}
