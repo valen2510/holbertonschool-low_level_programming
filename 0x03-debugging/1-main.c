9cb0d0712c9e02f00f7c96210569019de8fc8d11
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-16 22:03:24 -0500

    commentloop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,6 +3,7 @@
 /**
  * main - causes an infinite loop
  * Return: 0
+ * \\o/\n - emoji
  */
 
 int main(void)
@@ -10,14 +11,14 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
-	i = 0;
-/**
+/*
+*	i = 0;
+*
 *       while (i < 10)
 *        {
 *                putchar(i);
 *        }
-**/
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
