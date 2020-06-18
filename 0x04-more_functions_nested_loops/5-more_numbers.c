e7ee4952f36301b2a47ff7b00a407b18ec32caa5
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-18 14:37:23 -0500

    more_n

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * more_numbers - print lines of n
+ *
+ *
+ */
+void more_numbers(void)
+{
+	char f, n;
+
+	f = 0;
+	while (f <= 10)
+	{
+		n = 0;
+		while (n <= 14)
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+
+			n++;
+		}
+		_putchar('\n');
+		f++;
+	}
+}
+
