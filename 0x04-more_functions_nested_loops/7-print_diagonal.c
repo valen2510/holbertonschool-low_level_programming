1f212c3aa422ad473fc076bb4e4ea7e7419bdef7
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-18 17:46:55 -0500

    print_diag

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * print_diagonal - print n tines a \
+ * @n: times
+ * Return: void
+ **/
+void print_diagonal(int n)
+{
+	int i, e;
+
+	i = 0;
+	while (i < n)
+	{
+		if (n > 0)
+		{
+			e = 0;
+			while (e < i)
+			{
+				_putchar(' ');
+				e++;
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+
+		i++;
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
+
