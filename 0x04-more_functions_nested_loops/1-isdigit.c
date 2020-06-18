7f1ab441fcc7f71ec7e609132c784efb6c913f9c
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-18 13:27:52 -0500

    check_digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isdigit - check for a digit (0 to 9)
+ * @c: value
+ * Return: 1 (True) 0 (False)
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
