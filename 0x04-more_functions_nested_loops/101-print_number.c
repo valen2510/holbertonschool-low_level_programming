5606fe4cb1426a8f3ea29e93cd3d7c82606d3b22
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-20 16:12:24 -0500

    [File] Print integers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+/**
+ * print_number - print an integer
+ * @n: value
+ *
+ */
+void print_number(int n)
+{
+	unsigned int num;
+	int b, m;
+
+	if (n >= 0 && n < 10)
+	{
+		_putchar(n + 48);
+	}
+	else if (n < 0 && n > -10)
+	{
+		n *= -1;
+		_putchar('-');
+		_putchar(n + 48);
+	}
+	else 
+	{
+		if (n < 0)
+		{
+			n *= -1;
+			_putchar('-');
+		}
+		b = 10; /*base 10*/
+		num = n;
+		while (num / b > 9)
+		{
+			b = b * 10; /*mul 10 por base hasta encontrar unidad*/
+		}
+		while (b > 0)
+		{
+			m = num / b;
+			num = num % b;
+			_putchar( m + 48);
+			b = b / 10;
+		}
+	}
+}
+
+
+
+
+
