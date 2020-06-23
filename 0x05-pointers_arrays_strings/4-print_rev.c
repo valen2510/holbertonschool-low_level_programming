2257b6fadd887fc311a89caf91edc9ba15261d2f
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-22 22:23:15 -0500

    [File] Print reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_rev - print a string in reverse
+ * @s: array
+ *
+ */
+void print_rev(char *s)
+{
+	char i1, i2;
+
+	i1 = 0;
+	while (*(s + i1) != '\0')
+	{
+		i1++;
+	}
+
+	i2 = i1 - 1;
+	while (i2 >= 0)
+	{
+		_putchar(*(s + i2));
+		i2--;
+	}
+
+	_putchar('\n');
+}
