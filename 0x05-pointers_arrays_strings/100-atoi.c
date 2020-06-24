fa08f70f0d444e55221ec4caa0bdb8871d8dcbba
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-24 14:45:41 -0500

    [File] String to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _atoi - convert sring into integer
+ * @s: pointer
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int sign = 0;
+	int r = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+			sign++;
+
+		else if (*s >= '0' && *s <= '9')
+			r = r * 10 + (*s - '0');
+
+		else if (r > 0)
+			break;
+		s++;
+	}
+	if (sign % 2 != 0)
+		return (r * -1);
+	return (r);
+	}
+}
