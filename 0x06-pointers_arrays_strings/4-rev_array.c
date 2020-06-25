550fdc02d3c61b8218d6289ed05f6cfda6802044
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-25 18:50:46 -0500

    [File] Reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * reverse_array - reverse array of integers
+ * @n: elements
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i, t;
+
+	i = 0;
+
+	while (i < n - 1)
+	{
+		t = a[i];
+		a[i] = a[n-1];
+		a[n-1] = t;
+		n--;
+		i++;
+	}
+
+}
