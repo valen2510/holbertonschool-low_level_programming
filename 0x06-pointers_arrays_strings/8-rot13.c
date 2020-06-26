0f3be39a04f71781296374de7289afc2a4b92855
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-25 23:51:22 -0500

    [File] Encode to rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * rot13 - encode to rot13
+ * @a: string
+ * Return: result encoded
+ */
+char *rot13(char *a)
+{
+	int i, j;
+	char f[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char s[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; f[j]; j++)
+		{
+			if (a[i] == f[j])
+				a[i] = s[j];
+
+		}
+	}
+	return (a);
+}
+
