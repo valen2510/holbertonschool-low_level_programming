46483dfa446316bf1fb546780c4983f24dff1529
valen2510 <valenjaramillo1025@hotmail.com>
2020-06-29 19:13:19 -0500

    [File] Print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_chessboard - print chessboard
+ * @a: pointer to 2D-array
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < 8)
+	{
+		j = 0;
+
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
