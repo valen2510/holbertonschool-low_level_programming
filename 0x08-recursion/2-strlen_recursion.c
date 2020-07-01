f56930aff12ad61496e0b3bcfe73eab46391d388
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-01 17:01:01 -0500

    [File] Length String

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - Lenght of a string
+ * @s: String
+ **/
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s)
+	{
+		count = _strlen_recursion(s + 1);
+		count++;
+	}
+	return (count);
+}
