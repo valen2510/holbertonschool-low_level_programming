fa2b3de00ff7257123dee0c251fd494e15303af9
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-04 22:45:46 -0500

    [File] Add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+int main(int argc, char **argv)
+{
+	int i, n;
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		n = atoi(argv[i]);
+
+		if (n > 0)
+			sum += n;
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	
+	printf("%d\n", sum);
+	
+	return (0);
+}
