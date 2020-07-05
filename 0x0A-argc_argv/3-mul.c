a3fa72c89662b0c315307a669f3c2d30381d93f1
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-04 21:44:25 -0500

    [File] Multiplie two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry point. Multiplication of two numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 (Success) 1 (Error)
+ */
+int main(int argc, char **argv)
+{
+	int n1, n2;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+	}
+
+	printf("%d\n", n1 * n2);
+	return (0);
+}
+
