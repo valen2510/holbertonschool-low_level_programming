0249fc8f90e3db19aa44a3da38bc469e5481f0e7
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-07 22:48:00 -0500

    [File] Concatanates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,11 +11,11 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, k, l, count;
 	char *p;
 
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+
+	else if (s2 == NULL)
 		s2 = "";
-	}
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
