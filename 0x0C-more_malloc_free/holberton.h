a62c9420ebf398ce62b35d0ad2b1779ffa08f72c
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-09 17:27:03 -0500

    [File] Prototypes

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,6 +1,7 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+unsigned int length(char *s);
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
