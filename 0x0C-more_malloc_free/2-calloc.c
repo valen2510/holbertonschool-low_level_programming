a76e3fdde19b3ffa24362c8764bc4ae2879d1bd1
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-09 17:30:38 -0500

    [File] Allocate memory using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p = NULL;
 	unsigned int i = 0;
 
-	if (!nmemb || !size)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	p = malloc(size * nmemb);
