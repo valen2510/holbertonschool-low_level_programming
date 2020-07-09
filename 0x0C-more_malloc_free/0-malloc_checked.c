cf89f208a11fa50400e30964f56291cac6d62118
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-09 14:27:59 -0500

    [File] Allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * malloc_checked - allocate memory
+ * @b: size_t
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = NULL;
+	p = malloc(b);
+
+	if (!p)
+		exit(98);
+
+	return (p);
+}
