a91b13c0bbff469edd6ee7ddefcd8461d0ac54ba
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-13 11:42:30 -0500

    [File] Prototypes

---
+++ 0x0D-preprocessor/holberton.h
@@ -0,0 +1,20 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+unsigned int length(char *s);
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+int lenght(char **DoblePointer);
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif
