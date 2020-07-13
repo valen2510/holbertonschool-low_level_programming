10dda5c3c8ccc9f1502aa0e540052013a7afb1bd
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-13 17:45:17 -0500

    [File]Prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,21 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new struct for dog
+ * @name: pointer to dog name
+ * @age: dog age
+ * @owner: dog owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-unsigned int length(char *s);
-int *array_range(int min, int max);
-void *_calloc(unsigned int nmemb, unsigned int size);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *malloc_checked(unsigned int b);
-int lenght(char **DoblePointer);
-int _putchar(char c);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
+
 
 #endif
