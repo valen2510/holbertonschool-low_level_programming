c09c692bce170777ea4352e17a37697039f37337
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-13 23:45:02 -0500

    [File] Header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,8 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *copys(char *s1, char *s2);
 
 #endif
