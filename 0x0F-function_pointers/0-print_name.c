61687c8d681b7cfeb900ae4c7e5be49bed776a1c
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-15 12:37:42 -0500

    [File] Print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 /**
  * print_name - print a name
@@ -6,6 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name)
+	if (name && f)
 		f(name);
 }
