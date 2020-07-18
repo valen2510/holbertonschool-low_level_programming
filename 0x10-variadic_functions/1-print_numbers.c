9f24aaa4f1c868c25d271ad19bbd61ea1ec0f7ef
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-18 12:17:24 -0500

    [File] Fix separator requirement

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,18 +10,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
-	if (*separator && n)
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
 	{
-		va_start(list, n);
+		printf("%d", va_arg(list, int));
 
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(list, int));
-
-			if (i < (n - 1))
-				printf("%s", separator);
-		}
+		if (separator && i != (n - 1))
+			printf("%s", separator);
+	}
 		va_end(list);
 		printf("\n");
-	}
 }
