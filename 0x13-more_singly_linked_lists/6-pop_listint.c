918a64aac95b79c685064698802bd23081884a5c
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-27 23:00:09 -0500

    [File] Create delete head node function

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * pop_listint - delete the first node of a listit_t list
+ * @head: pointer to head pointer
+ * Return: deleted first node data (n)
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *aux_head;
+	int n;
+
+	aux_head = *head;
+	n = 0;
+
+	if ((*head)->next)
+	{
+		*head = (*head)->next;
+		n = aux_head->n;
+		free(aux_head);
+	}
+
+	return (n);
+}
