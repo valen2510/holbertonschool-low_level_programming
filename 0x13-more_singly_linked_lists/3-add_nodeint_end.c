13cd72ad5b50d31606a8172428f46f6b7f06f81b
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-27 22:55:57 -0500

    [File] Create add node init end function

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - add node at the end of the listint_t list
+ * @head: pointer to pointer head
+ * @n: integer
+ * Return: New_Node address
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *New_Node;
+	listint_t **aux_head;
+
+	New_Node = (listint_t *)malloc(sizeof(listint_t));
+
+	if (!New_Node)
+	{
+		return (NULL);
+	}
+
+	if (!head)
+	{
+		New_Node->n = n;
+		New_Node->next = *head;
+		*head = New_Node;
+	}
+
+	else
+	{
+		aux_head = head;
+
+		while (*aux_head)
+		{
+			aux_head = &(*aux_head)->next;
+		}
+
+		New_Node->n = n;
+		New_Node->next = *aux_head;
+		*aux_head = New_Node;
+
+	}
+
+	return (New_Node);
+}
