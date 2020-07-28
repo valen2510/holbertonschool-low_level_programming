f4a4ed4d4c20ae20d0e84f8634e9f75ac955289f
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-27 22:57:58 -0500

    [File] Create free list 2 function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint2 - free a listint list
+ * @head: pointer to head pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *Current_Node;
+
+	while (*head)
+	{
+		Current_Node = *head;
+		*head = Current_Node->next;
+		free(Current_Node);
+	}
+}
