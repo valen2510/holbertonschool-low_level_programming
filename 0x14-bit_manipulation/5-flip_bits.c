5df0cf61560eb5426ec156bb20c238000f0c873d
valen2510 <valenjaramillo1025@hotmail.com>
2020-07-31 23:06:40 -0500

    Create number of bits to flip from one number to another function

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * flip_bits - find number of bits to flip from one number to another
+ * @n: first integer
+ * @m: second integer
+ * Return: (n_bits) number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int bit_difference, n_bits;
+
+	bit_difference = n ^ m;
+	n_bits = 0;
+
+	while (bit_difference > 0)
+	{
+		if (bit_difference & 1)
+			n_bits++;
+
+		bit_difference >>= 1;
+	}
+
+	return (n_bits);
+}
