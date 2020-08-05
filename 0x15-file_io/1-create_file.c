270fd9415d6c6cc83565f3b1ed1a21c7fcd662cb
valen2510 <valenjaramillo1025@hotmail.com>
2020-08-05 13:36:31 -0500

    [Header] Change creat for open and add new condition text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,19 +12,16 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 	return (-1);
 
-	file_descriptor = creat(filename, 0600);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file_descriptor == -1)
 		return (-1);
 
-	file_descriptor = open(filename, O_WRONLY);
-
-	if (file_descriptor == -1)
-		return (-1);
-
-	length = _lenstring(text_content);
-
-	file_d = write(file_descriptor, text_content, length);
+	if (text_content)
+	{
+		length = _lenstring(text_content);
+		file_d = write(file_descriptor, text_content, length);
+	}
 
 	if (file_d == -1)
 		return (-1);
