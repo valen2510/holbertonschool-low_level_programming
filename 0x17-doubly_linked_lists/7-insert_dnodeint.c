0f23d14e70cf68ffc0b80acaf7240c999f7dcc4a
valen2510 <valenjaramilo1025@hotmail.com>
2020-09-05 19:09:22 +0000

    Change conditions at the start of the list and at eh end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,29 +12,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node = NULL;
 	unsigned int i;
 
-	if (idx > dlistint_len(current_node) + 1)
+	if (idx > dlistint_len(current_node))
 		return (NULL);
+
+	if (idx == 0 || *h == NULL)
+		return (add_dnodeint(h, n));
+
+	if (idx == dlistint_len(*h))
+		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
-	for (i = 0; i < idx - 1 && idx != 0; i++)
+	for (i = 0; i < idx - 1; i++)
 		current_node = current_node->next;
 
-	if (i == 0)
-	{
-		new_node->next = current_node;
-		new_node->prev = current_node->prev;
-		current_node->prev = new_node;
-		*h  = new_node;
-		return (new_node);
-	}
-
-	if (current_node->next)
-		current_node->next->prev = new_node;
-
+	current_node->next->prev = new_node;
 	new_node->next = current_node->next;
 	current_node->next = new_node;
 	new_node->prev = current_node;
@@ -56,3 +51,4 @@ size_t dlistint_len(const dlistint_t *h)
 	}
 	return (elements_number);
 }
+
