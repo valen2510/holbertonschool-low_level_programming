e1128d22ae13e6feae0f970f9f8e8a7119544670
valen2510 <valenjaramilo1025@hotmail.com>
2020-09-05 16:29:11 +0000

    Improve condition when index is not found

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,12 +9,28 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 
+	if (index > (dlistint_len(head) - 1))
+		return (NULL);
+
 	for (i = 0; i < index; i++)
-	{
 		head = head->next;
-		if (!head)
-			return (NULL);
-	}
 
 	return (head);
 }
+/**
+* dlistint_len - number of elements on a dlistint_t list
+* @h: head of the list
+* Return: elements number
+**/
+size_t dlistint_len(const dlistint_t *h)
+{
+	unsigned int elements_number = 0;
+
+	while (h)
+	{
+		elements_number++;
+		h = h->next;
+	}
+	return (elements_number);
+}
+
