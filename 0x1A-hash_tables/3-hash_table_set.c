f06037ba36643f07e4c3618d4b5684a8c29b2a9e
valen2510 <valenjaramillo1025@hotmail.com>
2020-10-02 19:08:52 +0000

    Add changes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *element = NULL, *match = NULL, *aux = NULL;
 	unsigned long int index = 0;
 
-	if (!ht || !key || !(*key) || !value)
+	if (!ht || !(ht->array) || !key || !(*key) || !value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (!index)
