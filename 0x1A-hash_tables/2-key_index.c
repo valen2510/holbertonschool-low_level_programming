1c4b9a8d55a014bc57720b4ae719fd887d618324
valen2510 <valenjaramillo1025@hotmail.com>
2020-10-02 01:51:03 +0000

    Function to retrieve the index where the key/value should be stored in the array

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+ * key_index - function to get the index of a key
+ * @key: key of the value
+ * @size: size of the pointers array
+ * Return: index at the key/value should be stored
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (!key || !size)
+		return (0);
+	return ((hash_djb2(key)) % size);
+}
