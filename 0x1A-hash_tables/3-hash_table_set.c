cc1b5fb7588084da1745a199ebe5ebc3ec315f4d
valen2510 <valenjaramillo1025@hotmail.com>
2020-10-02 16:39:30 +0000

    Change conditions for array not NULL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,7 +9,6 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *element = NULL;
-	hash_node_t *current = NULL;
 	unsigned long int index = 0;
 
 	if (!ht || !key || !value || !(*key))
@@ -28,36 +27,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!(element->key) || !(element->value))
 		return (0);
 
-	if ((match_element(key, ht->array[index])) == 0 || ht->array[index] != NULL)
+	if (ht->array[index] != NULL)
 	{
-		current = ht->array[index];
-		while (current->next)
-			current = current->next;
-
-		element->next = current->next;
-		current->next = element;
+		element->next = ht->array[index];
+		ht->array[index] = element;
 		return (1);
 	}
 	element->next = NULL;
 	ht->array[index] = element;
-	return (1);
-}
-/**
- * match_element - Check if a key value is already in a bucket.
- * @key: key
- * @array: bucket
- * Return: 1(No match Found) 0 (Match Found)
- */
-int match_element(const char *key, hash_node_t *array)
-{
-	hash_node_t *match = array;
-
-
-	while (match)
-	{
-		if (strcmp(match->key, key) == 0)
-			return (0);
-		match = match->next;
-	}
 	return (1);
 }
