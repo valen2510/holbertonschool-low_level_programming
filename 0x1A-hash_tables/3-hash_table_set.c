1a531fc49c4f0122df0c76fb15b5637adb1c9b7b
valen2510 <valenjaramillo1025@hotmail.com>
2020-10-02 17:07:29 +0000

    Add match_function apart

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,6 +10,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *element = NULL;
 	unsigned long int index = 0;
+	int match;
 
 	if (!ht || !key || !value || !(*key))
 		return (0);
@@ -18,16 +19,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!index)
 		return (0);
 
+	match = match_element(key, ht->array[index], value);
+	if (match == -1)
+		return (0);
+	if (!match)
+		return (1);
+
 	element = malloc(sizeof(hash_node_t));
 	if (!element)
 		return (0);
-
 	element->key = strdup(key);
 	element->value = strdup(value);
 	if (!(element->key) || !(element->value))
 		return (0);
 
-	if (ht->array[index] != NULL)
+	if (ht->array[index])
 	{
 		element->next = ht->array[index];
 		ht->array[index] = element;
@@ -35,5 +41,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	element->next = NULL;
 	ht->array[index] = element;
+	return (1);
+}
+/**
+ * match_element - Check if a key value is already in a bucket.
+ * @key: key
+ * @array: bucket
+ * @value: node value to update
+ * Return: 1(No match Found) 0 (Match Found)
+ */
+int match_element(const char *key, hash_node_t *array, const char *value)
+{
+	hash_node_t *match = array;
+
+	while (match)
+	{
+		if (strcmp(match->key, key) == 0)
+		{
+			match->value = strdup(value);
+			if (!(match->value))
+				return (-1);
+			return (0);
+		}
+		match = match->next;
+	}
 	return (1);
 }
