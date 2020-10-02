ddd56cb19776dea1bc126b27a6fdab6b5c9e68e7
valen2510 <valenjaramillo1025@hotmail.com>
2020-10-02 20:36:46 +0000

    Add hash table non existent

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,6 +8,9 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i, comma = 0;
 	hash_node_t *current = NULL;
 
+	if (ht == NULL)
+		return;
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
