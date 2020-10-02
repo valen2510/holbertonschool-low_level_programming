ea5120c2672dab95a499864f44024df837c0f99f
valen2510 <valenjaramillo1025@hotmail.com>
2020-10-02 19:13:12 +0000

    Add changes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,8 +14,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !(ht->array) || !key || !(*key) || !value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	if (!index)
-		return (0);
 	aux = ht->array[index];
 	if (aux)
 	{
