2fdefeec4a34d4308ce4ee6810080edebc69e5fd
valen2510 <valenjaramillo1025@hotmail.com>
2020-10-02 01:54:23 +0000

    Add size 0 case

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
 
+	if (!size)
+		return (NULL);
+
 	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
 		return (NULL);
