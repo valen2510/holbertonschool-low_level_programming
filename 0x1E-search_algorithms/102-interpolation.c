94bc00d4df412781d2e9a32a77a3d6e717ef9535
valen2510 <valenjaramillo1025@hotmail.com>
2021-01-14 23:56:29 +0000

    | 01 | Search for a value in an array with interpolation search. |

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+/**
+ * interpolation_search - function that searches for a value
+ * in an array of integers using the interpolation search.
+ * @array: array of integers.
+ * @size: size of array.
+ * @value: search value.
+ * Return: index of search value in array or -1 if doesn't exist in array.
+*/
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low = 0, high = size - 1;
+	size_t pos = low + (((double)(high - low) /
+	(array[high] - array[low])) * (value - array[low]));
+
+	if (!array)
+		return (-1);
+
+	while (low < high)
+	{
+		if (value > array[high] ||  value < array[low])
+		{
+			printf("Value checked array[%lu] is out of range\n", pos);
+			break;
+		}
+
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+
+		if (array[pos] == value)
+			return ((int)pos);
+		else if (array[pos] > value)
+			high = pos - 1;
+		else
+			low = pos + 1;
+
+		pos = low + (((double)(high - low) /
+		(array[high] - array[low])) * (value - array[low]));
+	}
+	return (-1);
+}
